5a64b6f8eea7f1763d7d26bdbbcda8f8f7d2ae4a
강인수 <boykis82@gmail.com>
2021-04-20 17:45:22 +0900

    모듈 & 임베딩 & 모델 분리

---
+++ LossLogger.py
@@ -0,0 +1,16 @@
+from gensim.models.callbacks import CallbackAny2Vec
+
+# gensim으로 word2vec 훈련 중 epoch마다 loss를 확인할 수 있는 callback
+class LossLogger(CallbackAny2Vec):
+    def __init__(self):
+        self.epoch = 1
+        self.loss_previous_step = 0
+
+    def on_epoch_begin(self, model):
+        print(f'Epoch: {self.epoch}', end='\t')
+
+    def on_epoch_end(self, model):
+        loss = model.get_latest_training_loss()
+        print(f'Loss after epoch {self.epoch}: {loss - self.loss_previous_step}')
+        self.epoch += 1
+        self.loss_previous_step = loss        
